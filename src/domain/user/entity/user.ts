3cf06914cd8836ef75649c6290fe2a200a503d60
Roniery Abreu <heroniery007@gmail.com>
2025-07-22 23:39:03 -0300

    updates user entity to uuid id

--- src/domain/user/entity/user.ts
+++ src/domain/user/entity/user.ts
@@ -1,20 +1,34 @@
 export type UserProps = {
+    id: string
     name: string;
     email: string;
-    password: string;
 }
 
 
 export class User{
     private constructor(private props: UserProps){}
 
-    public create(name: string, email: string, password: string){
+    public static create(name: string, email: string){
         return new User({
+            id: crypto.randomUUID().toString(),
             name, 
             email,
-            password
         })
     }
 
-    
+    public static with(props : UserProps) : User{
+        return new User(props)
+    }
+
+    public get id() : string {
+        return this.props.id;
+    }
+
+    public get name() : string{
+        return this.props.name
+    }
+
+    public get email() : string {
+        return this.props.email
+    }
 }
