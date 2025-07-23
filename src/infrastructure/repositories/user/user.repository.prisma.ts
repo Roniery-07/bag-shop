d497b9a63036a94e598f614ab4bcc8dde70899a9
Roniery Abreu <heroniery007@gmail.com>
2025-07-22 23:39:42 -0300

    implements basic prisma repo implementation

---
+++ src/infrastructure/repositories/user/user.repository.prisma.ts
@@ -0,0 +1,36 @@
+import { User } from "@/domain/user/entity/user";
+import { UserGateway } from "@/domain/user/gateway/user.gateway";
+import { PrismaClient } from "@/generated/prisma";
+
+export class UserRepositoryPrisma implements UserGateway{
+    private constructor(private readonly prismaClient : PrismaClient){}
+
+    public static create(prismaClient: PrismaClient){
+        return new UserRepositoryPrisma(prismaClient)
+    }
+
+    public async save(user: User) : Promise<void>{
+        const data = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+        }
+
+        await this.prismaClient.user.create({data})
+    }
+
+    public async list() : Promise<User[]> {
+        const users = await this.prismaClient.user.findMany();
+
+        const usersList = users.map(u => {
+            const user = User.with({
+                id: u.id,
+                email: u.email,
+                name: u.name ?? "",
+            })
+            return user;
+        })
+
+        return usersList
+    }
+}
