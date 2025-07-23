2955643acc5b5703fcfb211fe2993753bcc0bd8e
Roniery Abreu <heroniery007@gmail.com>
2025-07-22 22:41:52 -0300

    implements user basic usecases

---
+++ src/usecases/user/create-user-usecases.ts
@@ -0,0 +1,39 @@
+import { User } from "@/domain/user/entity/user";
+import { Usecase } from "../usecases";
+import { UserGateway } from "@/domain/user/gateway/user.gateway";
+
+export type CreateUserInputDto = {
+    name: string;
+    email: string;
+    password: string
+}
+
+export type CreateUserOutputDto = {
+    id: string
+}
+
+export class CreateUserUsecases implements Usecase<CreateUserInputDto, CreateUserOutputDto>{
+    private constructor(private readonly userGateway : UserGateway){}
+
+    public static create(userGateway: UserGateway){
+        return new CreateUserUsecases(userGateway)
+    }
+
+    public async execute({
+        name,
+        email,
+    } : CreateUserInputDto) : Promise<CreateUserOutputDto> {
+        const user = User.create(name, email)
+
+        await this.userGateway.save(user)
+        return this.presentOutput(user);
+    }
+
+    private presentOutput(user: User) : CreateUserOutputDto{
+        const output : CreateUserOutputDto = {
+            id: user.id
+        }
+        return output
+    }
+
+}
---
+++ src/usecases/user/list-user-usecases.ts
@@ -0,0 +1,47 @@
+import { User } from "@/domain/user/entity/user"
+import { Usecase } from "../usecases"
+import { UserGateway } from "@/domain/user/gateway/user.gateway"
+
+export type ListUserInputDto = void
+
+export type ListUserOutputDto = {
+    users: {
+        id: string;
+        name: string;
+        email: string;
+    }[];
+}
+
+export class ListUserUsecases implements Usecase<ListUserInputDto, ListUserOutputDto>{
+    private constructor(private readonly userGateway : UserGateway){}
+
+    public static create(userGateway: UserGateway){
+        return new ListUserUsecases(userGateway)
+    }
+
+    public async execute() : Promise<ListUserOutputDto>{
+
+        // faz sentido que o metodo list não tenha envolvimento do dominio User
+        // já que não faz sentido no sentido de regra de negocio do user fazer um list
+        // dele mesmo, já que ele é apenas a representacao de um indivíduo
+
+        const users = await this.userGateway.list();
+
+        return this.presentOutput(users)
+    }
+
+
+    // em presentOutput estou transformando a entidade em DTO que será exposta para as camadas
+    // mais externas
+    private presentOutput(users: User[]) : ListUserOutputDto{
+        return {
+            users: users.map(u => {
+                return {
+                    id: u.id,
+                    name: u.name,
+                    email: u.email
+                }
+            })
+        }   
+    }
+}
