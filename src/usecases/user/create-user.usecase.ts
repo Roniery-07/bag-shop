bac15299f622bc2c3008a05c5da9051bef405e5f
Roniery Abreu <heroniery007@gmail.com>
2025-08-21 22:58:17 -0300

    fix create user usecase file name and class name

--- src/usecases/user/create-user.usecase.ts
+++ src/usecases/user/create-user.usecase.ts
@@ -1,6 +1,8 @@
 import { User } from "@/domain/model/user/entity/user";
 import { Usecase } from "../usecases";
 import { UserGateway } from "@/domain/model/user/gateway/user.gateway";
+import { Cart } from "@/domain/model/cart/entity/cart";
+import { CartGateway } from "@/domain/model/cart/gateway/cart.gateway";
 
 export type CreateUserInputDto = {
     name: string;
@@ -12,20 +14,27 @@ export type CreateUserOutputDto = {
     id: string
 }
 
-export class CreateUserUsecases implements Usecase<CreateUserInputDto, CreateUserOutputDto>{
-    private constructor(private readonly userGateway : UserGateway){}
+export class CreateUserUsecase implements Usecase<CreateUserInputDto, CreateUserOutputDto>{
+    private constructor(
+        private readonly userGateway : UserGateway,
+        private readonly cartGateway : CartGateway
+    ){}
 
-    public static create(userGateway: UserGateway){
-        return new CreateUserUsecases(userGateway)
+    public static create(userGateway : UserGateway, cartGateway : CartGateway){
+        return new CreateUserUsecase(userGateway, cartGateway)
     }
 
     public async execute({
         name,
         email,
     } : CreateUserInputDto) : Promise<CreateUserOutputDto> {
-        const user = User.create(name, email)
 
+        const user = User.create(name, email);
+        const cart = Cart.create(user.id);
+
+        await this.cartGateway.save(cart)
         await this.userGateway.save(user)
+        
         return this.presentOutput(user);
     }
 
