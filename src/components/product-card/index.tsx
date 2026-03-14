4dfec8001b78ad71a0efd5dde7f63e9f47894808
Roniery-07 <heroniery007@gmail.com>
2026-03-14 19:40:34 -0300

    fix slider behavior and design

--- src/components/product-card/index.tsx
+++ src/components/product-card/index.tsx
@@ -56,8 +56,7 @@ export default function ProductCard({ product }: Props) {
     });
 
   return (
-    <article className="group relative w-full max-w-[220px] overflow-hidden rounded-2xl bg-white shadow transition hover:shadow-lg">
-      {/* IMAGEM */}
+    <article className="group mx-auto relative mb-2 max-w-[220px] overflow-hidden rounded-2xl bg-white shadow transition hover:shadow-lg">
       <Link href={`/products/${product.id}`} className="block">
         <div className="relative aspect-square w-full bg-pink-100">
           {cover && (
