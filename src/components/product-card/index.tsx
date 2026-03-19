16b711452ea62d7fc929adfe004f988edbddf499
Roniery-07 <heroniery007@gmail.com>
2026-03-19 11:32:02 -0300

    adds link to the entire product card and fix the overlap between cart button and product link

--- src/components/product-card/index.tsx
+++ src/components/product-card/index.tsx
@@ -5,20 +5,12 @@ import { Heart, ShoppingCart } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
+import { GetProductOutputDto } from '@/usecases/product/get-product.usecases';
 
 import { useAuth } from '@/lib/context/authContext';
 
-type ProductImage = { url: string; order?: number };
-export interface Product {
-  id: string;
-  name: string;
-  price: number; // em centavos? ajuste conforme
-  oldPrice?: number;
-  images: ProductImage[];
-}
-
 interface Props {
-  product: Product;
+  product: GetProductOutputDto;
 }
 
 export default function ProductCard({ product }: Props) {
@@ -26,7 +18,12 @@ export default function ProductCard({ product }: Props) {
   const pathname = usePathname();
   const { user } = useAuth();
 
-  const handleAddToCartClick = async (productId: string, quantity: number) => {
+  const handleAddToCartClick = async (
+    e,
+    productId: string,
+    quantity: number,
+  ) => {
+    e.stopPropagation();
     if (!user) {
       router.push(`auth/login?callbackUrl=${encodeURIComponent(pathname)}`);
       return;
@@ -56,7 +53,7 @@ export default function ProductCard({ product }: Props) {
     });
 
   return (
-    <article className="group mx-auto w-full relative mb-2 max-w-[220px] overflow-hidden rounded-2xl bg-white shadow transition hover:shadow-lg">
+    <article className="group mx-auto w-full relative mb-2 max-w-[220px] overflow-hidden rounded-2xl bg-white shadow transition ">
       <Link href={`/products/${product.id}`} className="block">
         <div className="relative aspect-square w-full bg-pink-100">
           {cover && (
@@ -65,39 +62,38 @@ export default function ProductCard({ product }: Props) {
               alt={product.name}
               fill
               sizes="220px"
-              className="object-cover transition-transform duration-500 group-hover:scale-105"
+              className="object-cover transition-transform duration-500 "
             />
           )}
         </div>
-      </Link>
 
-      {/* INFO */}
-      <div className="flex flex-col gap-1 px-3 pb-4 pt-2">
-        <h3 className="line-clamp-2 h-10 text-sm font-medium text-pink-950">
-          {product.name}
-        </h3>
+        {/* INFO */}
+        <div className="flex flex-col gap-1 px-3 pb-4 pt-2">
+          <h3 className="line-clamp-2 h-10 text-sm font-medium text-pink-950">
+            {product.name}
+          </h3>
 
-        {/* preços */}
-        <div className="flex items-center gap-2">
-          <span className="text-base font-semibold text-pink-900">
-            {format(product.price)}
-          </span>
-          {product.oldPrice && (
-            <span className="text-xs text-pink-600 line-through">
-              {format(product.oldPrice)}
+          {/* preços */}
+          <div className="flex items-center gap-2">
+            <span className="text-base font-semibold text-pink-900">
+              {format(product.price)}
             </span>
-          )}
+            {/* {product.oldPrice && ( */}
+            {/*   <span className="text-xs text-pink-600 line-through"> */}
+            {/*     {format(product.oldPrice)} */}
+            {/*   </span> */}
+            {/* )} */}
+          </div>
         </div>
-      </div>
 
-      {/* ADD-TO-CART aparece no hover */}
-      <button
-        onClick={() => handleAddToCartClick(product.id, 1)}
-        className="absolute bottom-3 right-3 hidden items-center gap-1 rounded-full bg-pink-400 px-3 py-1.5 
-          text-xs font-semibold text-white shadow-md transition hover:bg-pink-500 group-hover:flex"
-      >
-        <ShoppingCart className="h-4 w-4" />
-      </button>
+        <button
+          onClick={(e) => handleAddToCartClick(e, product.id, 1)}
+          className="absolute bottom-3 right-3 hidden items-center gap-1 rounded-full bg-pink-400 px-3 py-1.5 
+          z-50 text-xs font-semibold text-white shadow-md transition hover:bg-pink-500 group-hover:flex"
+        >
+          <ShoppingCart className="h-4 w-4" />
+        </button>
+      </Link>
     </article>
   );
 }
